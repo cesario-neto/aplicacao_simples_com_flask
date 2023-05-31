a3dfdc5e0dccb13443ebb18af1473d5fc41adef6
Cesario_Neto <cesarioneto7@gmail.com>
2023-05-30 22:26:48 -0300

    Criando a lógica da soma e retornando resultado

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        
+        soma = int(num1) + int(num2)
+        
+        return render_template('index.html', resultado=soma)
+    
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
